d0d823fb3e3d2f726e43a3c151dec386360799a4
izu93 <karthik.burra@qlik.com>
2025-05-11 16:27:44 -0500

    configure data structure to add data props

--- hello/src/data.js
+++ hello/src/data.js
@@ -1,3 +1,7 @@
 export default {
-  targets: [],
+  targets: [
+    {
+      path: "/qHyperCubeDef",
+    },
+  ],
 };
--- hello/src/object-properties.js
+++ hello/src/object-properties.js
@@ -1,8 +1,8 @@
 const properties = {
-  showTitles: true,
-  title: '',
-  subtitle: '',
-  footnote: '',
+  qHyperCubeDef: {
+    qInitialDataFetch: [{ qWidth: 2, qHeight: 10 }],
+  },
+  // ...
 };
 
 export default properties;
