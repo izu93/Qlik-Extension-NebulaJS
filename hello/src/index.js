1b24bb808af286fc75c62439c07c276c975aae29
izu93 <karthik.burra@qlik.com>
2025-05-11 16:35:11 -0500

    Render data

--- hello/src/index.js
+++ hello/src/index.js
@@ -1,7 +1,8 @@
-import { useElement } from '@nebula.js/stardust';
-import properties from './object-properties';
-import data from './data';
-import ext from './ext';
+import { useLayout, useElement, useEffect } from "@nebula.js/stardust";
+import properties from "./object-properties";
+import data from "./data";
+import ext from "./ext";
+
 /**
  * Entrypoint for your sense visualization
  * @param {object} galaxy Contains global settings from the environment.
@@ -19,7 +20,38 @@ export default function supernova(galaxy) {
     ext: ext(galaxy),
     component() {
       const element = useElement();
-      element.innerHTML = '<div>Hello!</div>'; // eslint-disable-line
+      const layout = useLayout();
+      console.log(useLayout());
+
+      useEffect(() => {
+        if (layout.qSelectionInfo.qInSelections) {
+          // skip rendering when in selection mode
+          return;
+        }
+        const hc = layout.qHyperCube;
+
+        // headers
+        const columns = [...hc.qDimensionInfo, ...hc.qMeasureInfo].map(
+          (f) => f.qFallbackTitle
+        );
+        const header = `<thead><tr>${columns
+          .map((c) => `<th>${c}</th>`)
+          .join("")}</tr></thead>`;
+
+        // rows
+        const rows = hc.qDataPages[0].qMatrix
+          .map(
+            (row) =>
+              `<tr>${row.map((cell) => `<td>${cell.qText}</td>`).join("")}</tr>`
+          )
+          .join("");
+
+        // table
+        const table = `<table>${header}<tbody>${rows}</tbody></table>`;
+
+        // output
+        element.innerHTML = table;
+      }, [element, layout]);
     },
   };
 }
